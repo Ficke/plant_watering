691967dce29c5a10d84fc7d3e5a6fdc7f45f7d0f
Adam F <aficke@gmail.com>
2021-11-01 15:33:45 -0400

    updated model name from plants to plant

--- pyWater/watering/models.py
+++ pyWater/watering/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Plants(models.Model):
+class Plant(models.Model):
     plant_name = models.CharField(max_length=200)
     plant_type = models.CharField(max_length=200)
     added_date = models.DateTimeField('date added')
 
 class Watering(models.Model):
-    watered_plant = models.ForeignKey(Plants, on_delete=models.CASCADE)
+    watered_plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
     water_date = models.DateTimeField('date watered')
     water_id = models.IntegerField()
